3689e2c74b0a1818f4bf1551540f891be39f85c3
Anth0nYM <marquesanthony62@gmail.com>
2022-01-27 16:40:52 -0300

    descobrindo o JOptionpane e tentando fazer um menu para o usuário

--- src/Teste.java
+++ src/Teste.java
@@ -2,57 +2,50 @@ import javax.swing.JOptionPane;
 
 public class Teste {
     public static void main(String[] args) throws Exception {
-     while (true){
-             String opcao = JOptionPane.showInputDialog("O que você deseja?: \n1- Novo cliente \n2- Apagar cliente  \n3- Listar clientes \n4- Novo serviço \n5- Apagar serviço  \n6- Listar serviços \n7- Realizar serviço \n0- Encerrar");
-             if (opcao.equals("0")){
-                     break;
-             }
-            if (opcao.equals("1")){
-                JOptionPane.showMessageDialog(null, "Novo cliente criado!");
+        while (true) {
+            String opcao = JOptionPane.showInputDialog(
+                    "O que você deseja?: \n1- Novo cliente \n2- Apagar cliente  \n3- Listar clientes \n4- Novo serviço \n5- Apagar serviço  \n6- Listar serviços \n7- Realizar serviço \n0- Encerrar");
+            if (opcao.equals("0")) {
+                break;
             }
-            if (opcao.equals("2")){
+            if (opcao.equals("1")) {
+                String nomeCli = JOptionPane.showInputDialog(null, "Nome do cliente:");
+                String emailCli = JOptionPane.showInputDialog(null, "Email do Cliente:");
+                String sexoCli = JOptionPane.showInputDialog(null, "Sexo do Cliente:(M ou F)");
+                String numeroEnd = JOptionPane.showInputDialog(null, "Número da casa:");
+                Integer numeroconv = Integer.valueOf(numeroEnd);
+                String logradoruroCli = JOptionPane.showInputDialog(null,"Logradouro: ");
+                String cidadeCli = JOptionPane.showInputDialog(null,"Cidade:");
+                String bairroCli = JOptionPane.showInputDialog(null,"Bairro:");
+                String cepCli = JOptionPane.showInputDialog(null,"CEP:");
+                Integer cepConv = Integer.valueOf(cepCli);
+                String ufCli = JOptionPane.showInputDialog(null,"UF:");
+                Cliente cliente = new Cliente(nomeCli, emailCli, sexoCli, new Endereco(numeroconv,logradoruroCli, cidadeCli,bairroCli, cepConv, ufCli));
+                System.out.println(cliente.getNomeCliente());
+            }
+            if (opcao.equals("2")) {
                 JOptionPane.showMessageDialog(null, "Cliente apagado!");
             }
-            if (opcao.equals("3")){
+            if (opcao.equals("3")) {
                 JOptionPane.showMessageDialog(null, "Clientes listados!");
             }
-            if (opcao.equals("4")){
-                JOptionPane.showMessageDialog(null, "Serviço criado!");
+            if (opcao.equals("4")) {
+                String nomeServ = JOptionPane.showInputDialog(null, "Nome do serviço:");
+                String descServ = JOptionPane.showInputDialog(null, "Descrição do serviço:");
+                String valorServ = JOptionPane.showInputDialog(null, "Valor do serviço:");
+                Integer servConv = Integer.valueOf(valorServ);
+                Servico servico = new Servico(nomeServ,descServ,servConv);
+                System.out.println(servico.getNomeServico());
             }
-            if (opcao.equals("5")){
+            if (opcao.equals("5")) {
                 JOptionPane.showMessageDialog(null, "Serviço apagado!");
             }
-            if (opcao.equals("6")){
+            if (opcao.equals("6")) {
                 JOptionPane.showMessageDialog(null, "Serviços listados!");
             }
-            if (opcao.equals("7")){
+            if (opcao.equals("7")) {
                 JOptionPane.showMessageDialog(null, "Serviço realizado!");
-        //Servico servico1 = new Servico("Maquiagem",
-        //        "aplicação de produtos com efeito cosmético,de embelezamento, ou disfarce.",
-        //        300.00);
-        //System.out.println(servico1.getNomeServico());
-        //Servico servico2 = new Servico("Depilação completa",
-        //        "extração de pelos temporária ou permanente.",
-        //        55.00);
-        //System.out.println(servico2.getNomeServico());
-        //Servico servico3 = new Servico("Corte De cabelo",
-        //        "Corte simples de cabelo.",
-        //        20.00);
-        //System.out.println(servico3.getNomeServico());
-        // String nomeCli = JOptionPane.showInputDialog("Digite seu nome completo");
-        // String emailCli = JOptionPane.showInputDialog("Digite seu e-mail");
-        // String sexoCli = JOptionPane.showInputDialog("Digite seu sexo (M ou F)");
-        // System.out.println("Building...");
-        // Cliente cliente1 = new Cliente("Anthony", "marquesanthony62@gmail.com", "M",
-        // new Endereco(12, "Rua projetata 00", "Picos", "Samambaia", 6460003, "PI"));
-        // System.out.println(cliente1.getNomeCliente());
-        // System.out.println(cliente1.getEmailCliente());
-        // System.out.println(cliente1.getSexoCliente());
-        // System.out.println(cliente1.getEnderecoCliente());
-
-        // System.out.println(servico1.getDescricaoServico());
-        // System.out.println(servico1.getValorServico());
+            }
+        }
     }
-}
-}
 }
